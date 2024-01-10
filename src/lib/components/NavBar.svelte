01fe2487fb21df179f1d52ca70981d28128af11b
pierre <pierremarien29@gmail.com>
2024-01-10 07:15:57 +0100

    premier essais d'import en suivant a la lettre les structures de dossiers proposer par sveltekit

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { assets } from "$app/paths";
+
+
+</script>
+<div class="navbar">
+    <div class="navbar__logo">
+        <img src="%sveltekit.assets%/images/logo.png" alt="logo" />
+    </div>
+    <div class="navbar__links">
+        <a href="#">Home</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </div>
+</div>
+
+<style lang="scss">
+
+</style>
